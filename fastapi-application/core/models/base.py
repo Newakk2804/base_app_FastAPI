d003ec930c3c5a03793847d86fcecc91f4744862
Newakk2804 <dan28.04@mail.ru>
2025-08-05 14:54:26 +0300

    added naming convertion in table user, and delete first migration

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,9 +1,13 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 from utils import camel_case_to_snake_case
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -5,3 +6,7 @@ from .base import Base
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (UniqueConstraint("foo", "bar"),)
