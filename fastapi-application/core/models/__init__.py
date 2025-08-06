0f1bd391f2ce72bb6cc7c409e8380be39f849dcb
Newakk2804 <dan28.04@mail.ru>
2025-08-06 13:07:09 +0300

    create migrate for users table

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "db_helper",
     "Base",
+    "User",
 )
 
-from .base import Base
-from .db_helper import db_helper
+from core.models.base import Base
+from core.models.db_helper import db_helper
+from core.models.user import User
