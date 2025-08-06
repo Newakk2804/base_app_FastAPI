1c606577b92d5857056f5f2617b9cde9eeafc89d
Newakk2804 <dan28.04@mail.ru>
2025-08-06 19:16:57 +0300

    applied isort to auth

--- fastapi-application/api/api_v1/auth.py
+++ fastapi-application/api/api_v1/auth.py
@@ -1,8 +1,8 @@
 from api.api_v1.fastapi_users import fastapi_users
 from api.dependencies.authentication.backend import authentication_backend
 from core.config import settings
-from fastapi import APIRouter
 from core.schemas.user import UserCreate, UserRead
+from fastapi import APIRouter
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
