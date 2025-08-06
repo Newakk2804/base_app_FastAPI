6e7249091861d1695f4ab6cca49605f0e785bc0d
Newakk2804 <dan28.04@mail.ru>
2025-08-06 12:13:52 +0300

    create id int pk mixin

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,11 +1,13 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+from core.config import settings
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
