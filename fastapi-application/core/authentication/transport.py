514ddc710904144ea5e2145dd1dbdcfb21fa4e00
Newakk2804 <dan28.04@mail.ru>
2025-08-06 18:24:18 +0300

    bearer transport path from config

--- fastapi-application/core/authentication/transport.py
+++ fastapi-application/core/authentication/transport.py
@@ -1,6 +1,7 @@
+from core.config import settings
 from fastapi_users.authentication import BearerTransport
 
 bearer_transport = BearerTransport(
     # TODO: update url
-    tokenUrl="auth/jwt/login",
+    tokenUrl=settings.api.bearer_token_url,
 )
