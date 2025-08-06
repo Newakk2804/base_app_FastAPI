d74db6102294026d7e0712d657f0f67ef6576565
Newakk2804 <dan28.04@mail.ru>
2025-08-06 13:50:48 +0300

    fix user model imports

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from core.models.base import Base
 from core.models.mixins.id_int_pk import IdIntPkMixin
 from core.types.user_id import UserIdType
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
