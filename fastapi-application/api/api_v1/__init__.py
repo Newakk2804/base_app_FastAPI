3b9cf34d5a2eafef06d672f4af5c794ea674dda4
Newakk2804 <dan28.04@mail.ru>
2025-08-06 16:38:47 +0300

    create auth router

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from api.api_v1.auth import router as auth_router
 from core.config import settings
-from api.api_v1.users import router as users_router
+from fastapi import APIRouter
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 
-router.include_router(users_router, prefix=settings.api.v1.users)
+router.include_router(auth_router)
---
+++ fastapi-application/api/api_v1/auth.py
@@ -0,0 +1,7 @@
+from core.config import settings
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix=settings.api.v1.auth,
+    tags=["auth"],
+)
