a96e677355527237e8429063629d624e5a610230
Newakk2804 <dan28.04@mail.ru>
2025-08-06 16:29:27 +0300

    create fastapi users routers helper

---
+++ fastapi-application/api/dependencies/fastapi_users.py
@@ -0,0 +1,10 @@
+from api.dependencies.backend import authentication_backend
+from api.dependencies.user_manager import get_user_manager
+from core.models import User
+from core.types.user_id import UserIdType
+from fastapi_users import FastAPIUsers
+
+fastapi_users = FastAPIUsers[User, UserIdType](
+    get_user_manager,
+    [authentication_backend],
+)
