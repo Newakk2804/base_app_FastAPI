aa1999c4d203c3874d072f8880f557eb219a4f45
Newakk2804 <dan28.04@mail.ru>
2025-08-06 13:46:52 +0300

    create access_token migration

---
+++ fastapi-application/alembic/versions/2025_08_06_1341-ec2ac3018a12_create_access_tokens_table.py
@@ -0,0 +1,55 @@
+"""create access_tokens table
+
+Revision ID: ec2ac3018a12
+Revises: fb74cab45056
+Create Date: 2025-08-06 13:41:39.961393
+
+"""
+
+from typing import Sequence, Union
+
+import fastapi_users_db_sqlalchemy
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "ec2ac3018a12"
+down_revision: Union[str, Sequence[str], None] = "fb74cab45056"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "access_tokens",
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            name=op.f("fk_access_tokens_user_id_users"),
+            ondelete="cascade",
+        ),
+        sa.PrimaryKeyConstraint("token", name=op.f("pk_access_tokens")),
+    )
+    op.create_index(
+        op.f("ix_access_tokens_created_at"),
+        "access_tokens",
+        ["created_at"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(
+        op.f("ix_access_tokens_created_at"),
+        table_name="access_tokens",
+    )
+    op.drop_table("access_tokens")
--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "AccessToken",
 )
 
+from core.models.access_token import AccessToken
 from core.models.base import Base
 from core.models.db_helper import db_helper
 from core.models.user import User
