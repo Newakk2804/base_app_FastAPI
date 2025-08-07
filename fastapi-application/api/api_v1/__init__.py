b2217cd860b3edeba7af8a3820416004e8cbaf46
Newakk2804 <dan28.04@mail.ru>
2025-08-07 13:24:14 +0300

    create messages router

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,8 +1,9 @@
-from fastapi.security import HTTPBearer
 from api.api_v1.auth import router as auth_router
+from api.api_v1.messages import router as messages_router
 from api.api_v1.users import router as users_router
 from core.config import settings
 from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 http_bearer = HTTPBearer(auto_error=False)
 
@@ -10,3 +11,4 @@ router = APIRouter(prefix=settings.api.v1.prefix, dependencies=[Depends(http_bea
 
 router.include_router(auth_router)
 router.include_router(users_router)
+router.include_router(messages_router)
